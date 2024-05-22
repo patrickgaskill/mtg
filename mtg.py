e3fe7f9f051e0eb85d0c63d0f64bf503684fef56
Patrick Gaskill <patrick@procurementsciences.com>
2024-05-22 14:30:41 -0400

    feat: add mana cost analysis

--- mtg.py
+++ mtg.py
@@ -1,10 +1,19 @@
 from datetime import datetime
+from pathlib import Path
 import orjson
 import pandas as pd
 import re
+import time
 
 with open("./data/default-cards-20240520210708.json", "r") as f:
     default_cards = orjson.loads(f.read())
+
+
+def make_output_dir():
+    run_time = time.strftime("%Y-%m-%d-%H%M%S")
+    output_path = Path("./data/output") / run_time
+    output_path.mkdir(parents=True, exist_ok=True)
+    return output_path
 
 
 def get_sort_key(card):
@@ -20,27 +29,124 @@ def get_sort_key(card):
     return release_date, card["set"], parsed_number, card["collector_number"]
 
 
-power_toughness_store = {}
+def generalize_mana_cost(mana_cost):
+    """
+    >>> generalize_mana_cost("{2}")
+    '{2}'
+    >>> generalize_mana_cost("{W}{W}")
+    '{M}{M}'
+    >>> generalize_mana_cost("{W}{U}{R}")
+    '{M}{N}{O}'
+    >>> generalize_mana_cost("{W}{U}{B}{R}{G}")
+    '{W}{U}{B}{R}{G}'
+    >>> generalize_mana_cost("{2/W}{2/U}")
+    '{2/M}{2/N}'
+    >>> generalize_mana_cost("{W/P}{W/U}{2/W}")
+    '{M/P}{M/N}{2/M}'
+    """
+    generics = iter("MNOP")
+    color_map = {}
 
-for card in default_cards:
-    power = card["power"] if "power" in card else None
-    toughness = card["toughness"] if "toughness" in card else None
-    if power is None and toughness is None:
-        continue
-    store_key = (power, toughness)
-    if store_key in power_toughness_store:
-        if get_sort_key(card) < get_sort_key(power_toughness_store[store_key]):
-            power_toughness_store[store_key] = card
-    else:
-        power_toughness_store[store_key] = card
+    for c in mana_cost:
+        if c in "WUBRG" and c not in color_map:
+            try:
+                color_map[c] = next(generics)
+            except StopIteration:
+                return mana_cost
 
-df = pd.DataFrame.from_dict(power_toughness_store, orient="index")
+    return mana_cost.translate(mana_cost.maketrans(color_map))
 
-output = (
-    df[["power", "toughness", "name", "set_name", "released_at"]]
-    .sort_values("released_at")
-    .reset_index(drop=True)
-)
 
-with open("./data/power_toughness.html", "w") as f:
-    output.to_html(f)
+class Store:
+    def __init__(self, name):
+        self._store = {}
+        self.name = name
+
+    def evaluate(self, card):
+        return self.add(card, None)
+
+    def add(self, card, key):
+        card["_sort_key"] = get_sort_key(card)
+
+        if key in self._store:
+            if card["_sort_key"] < self._store[key]["_sort_key"]:
+                self._store[key] = card
+                return True
+        else:
+            self._store[key] = card
+            return True
+
+        return False
+
+    def dataframe(self):
+        return pd.DataFrame.from_dict(self._store, orient="index")
+
+    def to_html(self, output_dir, columns):
+        with open(output_dir / f"{self.name}.html", "w") as f:
+            self.dataframe().sort_values("_sort_key")[columns].to_html(f)
+
+    def __len__(self):
+        return len(self._store)
+
+
+class PowerToughnessStore(Store):
+    def __init__(self, name="power_toughness"):
+        super().__init__(name)
+
+    def evaluate(self, card):
+        power = card["power"] if "power" in card else None
+        toughness = card["toughness"] if "toughness" in card else None
+
+        if power is None and toughness is None:
+            return False
+
+        key = (power, toughness)
+
+        return self.add(card, key)
+
+    def to_html(self, output_dir, columns=None):
+        columns = columns or ["power", "toughness", "name", "set_name", "released_at"]
+        return super().to_html(output_dir, columns)
+
+
+class ManaCostStore(Store):
+    def __init__(self, name="mana_cost"):
+        super().__init__(name)
+
+    def evaluate(self, card):
+        if "mana_cost" not in card:
+            return False
+
+        if "card_faces" in card:
+            card_was_added = False
+
+            for card_face in card["card_faces"]:
+                if "mana_cost" in card_face:
+                    general_mana_cost = generalize_mana_cost(card_face["mana_cost"])
+                    self.add(card, general_mana_cost)
+                    card_was_added = True
+
+            return card_was_added
+
+        general_mana_cost = generalize_mana_cost(card["mana_cost"])
+        return self.add(card, general_mana_cost)
+
+    def to_html(self, output_dir, columns=None):
+        columns = columns or ["mana_cost", "name", "set_name", "released_at"]
+        return super().to_html(output_dir, columns)
+
+
+def main():
+    stores = [PowerToughnessStore(), ManaCostStore()]
+
+    for card in default_cards:
+        for store in stores:
+            store.evaluate(card)
+
+    output_dir = make_output_dir()
+    for store in stores:
+        store.to_html(output_dir)
+
+
+if __name__ == "__main__":
+    main()
